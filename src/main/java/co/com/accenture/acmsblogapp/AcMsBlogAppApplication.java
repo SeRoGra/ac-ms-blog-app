8dc7a9f8890ff1168f79b11ee4e8ac9d266c0345
Sebastian Rodríguez <serogra15121999@gmail.com>
2022-12-25 18:14:37 -0500

    Commit inicial

---
+++ src/main/java/co/com/accenture/acmsblogapp/AcMsBlogAppApplication.java
@@ -0,0 +1,13 @@
+package co.com.accenture.acmsblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcMsBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcMsBlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/accenture/acmsblogapp/AcMsBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.accenture.acmsblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcMsBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
